295dc346a2ec72055be6efafe443c94485bf7ac2
unknown <govardhangn@gmail.com>
2026-02-16 05:13:29 +0530

    third commit

--- frontend/src/pages/CaseView.js
+++ frontend/src/pages/CaseView.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState, useCallback } from "react";
 import { useParams, useNavigate } from 'react-router-dom';
 import { caseAPI } from '../services/api';
 import {
